1dab0773701fa37745e9fed145463ee392099d90
Alex <t89137899903@gmail.com>
2018-02-12 21:14:26 +0300

    Лекция 1.4. Переменные и значения

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,6 +2,10 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
     public static void main(String[] args) {
-        System.out.println("Hello, beatch!");
+        System.out.println("Hello," + " beatch!");
+
+        int l = 8;
+        int s = l * l;
+        System.out.println("Площадь квадрата со стороной " + l + " = " + (s));
     }
 }
