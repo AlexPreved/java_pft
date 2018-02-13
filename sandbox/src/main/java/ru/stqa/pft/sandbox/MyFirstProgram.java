fe82bd9f970a33e417865482f0e6bebe964b8b0e
Alex <t89137899903@gmail.com>
2018-02-13 21:45:30 +0300

    Лекция про дебаг

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,11 +1,13 @@
 package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
-    public static void main(String[] args) {
-        System.out.println("Hello," + " beatch!");
 
-        int l = 8;
-        int s = l * l;
-        System.out.println("Площадь квадрата со стороной " + l + " = " + (s));
+    public static void main(String[] args) {
+      String somebody = "beatch!";
+      System.out.println("Hello," + somebody);
+
+      double l = 8.0;
+      double s = l * l;
+      System.out.println("Площадь квадрата со стороной " + l + " = " + (s));
     }
 }
