ce9355472d47f987ea8bd21ac80792e4c303825a
Alex <t89137899903@gmail.com>
2018-05-23 00:06:34 +0300

    Задание №4: Сделать тесты для создания групп и контактов в адресной книге
    сделал вроде

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/PersonData.java
@@ -0,0 +1,49 @@
+package ru.stqa.pft.addressbook;
+
+public class PersonData {
+  private final String first_name;
+  private final String middle_name;
+  private final String last_name;
+  private final String nickname;
+  private final String title;
+  private final String company;
+  private final String email;
+
+  public PersonData(String first_name, String middle_name, String last_name, String nickname, String title, String company, String email) {
+    this.first_name = first_name;
+    this.middle_name = middle_name;
+    this.last_name = last_name;
+    this.nickname = nickname;
+    this.title = title;
+    this.company = company;
+    this.email = email;
+  }
+
+  public String getFirst_name() {
+    return first_name;
+  }
+
+  public String getMiddle_name() {
+    return middle_name;
+  }
+
+  public String getLast_name() {
+    return last_name;
+  }
+
+  public String getNickname() {
+    return nickname;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public String getCompany() {
+    return company;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+}
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/UserCreationTests.java
@@ -0,0 +1,90 @@
+package ru.stqa.pft.addressbook;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.*;
+
+
+public class UserCreationTests {
+  FirefoxDriver wd;
+
+  @BeforeMethod
+  public void setUp() throws Exception {
+    wd = new FirefoxDriver();
+    wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    wd.get("http://localhost/addressbook/");
+    login("admin", "secret");
+  }
+
+  private void login(String username, String password) {
+    wd.findElement(By.name("user")).click();
+    wd.findElement(By.name("user")).clear();
+    wd.findElement(By.name("user")).sendKeys(username);
+    wd.findElement(By.name("pass")).click();
+    wd.findElement(By.name("pass")).clear();
+    wd.findElement(By.name("pass")).sendKeys(password);
+    wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+    wd.findElement(By.name("searchstring")).click();
+    wd.findElement(By.name("searchstring")).sendKeys("\\9");
+  }
+
+  @Test
+  public void testUserCreationTests() {
+    gotoNewPerson();
+    fillNewPerson(new PersonData("fAlex", "fSerg", "fPanchenko", "fprevedko", "mr", "aplana", "t89137899903@gmail.com"));
+    submitNewPerson();
+  }
+
+  private void submitNewPerson() {
+    wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+  }
+
+  private void fillNewPerson(PersonData personData) {
+    wd.findElement(By.name("firstname")).click();
+    wd.findElement(By.name("firstname")).clear();
+    wd.findElement(By.name("firstname")).sendKeys(personData.getFirst_name());
+    wd.findElement(By.name("middlename")).click();
+    wd.findElement(By.name("middlename")).clear();
+    wd.findElement(By.name("middlename")).sendKeys(personData.getMiddle_name());
+    wd.findElement(By.name("lastname")).click();
+    wd.findElement(By.name("lastname")).clear();
+    wd.findElement(By.name("lastname")).sendKeys(personData.getLast_name());
+    wd.findElement(By.name("nickname")).click();
+    wd.findElement(By.name("nickname")).clear();
+    wd.findElement(By.name("nickname")).sendKeys(personData.getNickname());
+    wd.findElement(By.name("title")).click();
+    wd.findElement(By.name("title")).clear();
+    wd.findElement(By.name("title")).sendKeys(personData.getTitle());
+    wd.findElement(By.name("company")).click();
+    wd.findElement(By.name("company")).clear();
+    wd.findElement(By.name("company")).sendKeys(personData.getCompany());
+    wd.findElement(By.name("address")).click();
+    wd.findElement(By.name("home")).click();
+    wd.findElement(By.name("email")).click();
+    wd.findElement(By.name("email")).clear();
+    wd.findElement(By.name("email")).sendKeys(personData.getEmail());
+  }
+
+  private void gotoNewPerson() {
+    wd.findElement(By.linkText("add new")).click();
+  }
+
+  @AfterMethod
+  public void tearDown() {
+    wd.quit();
+  }
+
+  public static boolean isAlertPresent(FirefoxDriver wd) {
+    try {
+      wd.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+}
