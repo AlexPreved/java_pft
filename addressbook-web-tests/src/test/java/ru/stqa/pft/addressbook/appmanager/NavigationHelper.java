addeac056d7f312e353de8b93d807c395bb9f1ea
Alex <t89137899903@gmail.com>
2018-05-29 00:14:58 +0300

    Лекция 2.8. Создаём общий базовый класс для классов-помощников

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -6,14 +6,14 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 /**
  * Created by pc05 on 27.05.2018.
  */
-public class NavigationHelper {
+public class NavigationHelper extends HelperBase{
   FirefoxDriver wd;
 
   public NavigationHelper(FirefoxDriver wd){
-    this.wd = wd;
+    super(wd);
   }
 
   public void gotoGroupPage() {
-    wd.findElement(By.linkText("groups")).click();
+    click(By.linkText("groups"));
   }
 }
